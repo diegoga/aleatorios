23e9d82cc9c168dd7c31611742604c7539654c5d
Diego González Álvarez <gadiego@gmail.com>
2015-05-29 11:19:47 +0200

    Aleatorios version 2

--- aleatorios.js
+++ aleatorios.js
@@ -1,9 +1,10 @@
 //Math.random(). Primer proyecto
 var numero = Math.random();
 
+var str = ' mayor que 0,5';
+
 if (numero<=0.5){
-	console.log('\n' + numero + ' menor que 0,5\n');
+	str= ' menor que 0,5';
 }
-else {
-	console.log('\n' + numero + ' mayor que 0,5\n')
-}
+	
+console.log('\n' + numero + str + '\n');
